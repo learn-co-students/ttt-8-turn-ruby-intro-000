889bb9a85c69fc157c92a126627df69f9621a074
aviflombaum <avi@flombaum.com>
2015-09-09 21:19:48 -0400

    starter specs

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,15 +8,20 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, input)
-  binding.pry
-  input.to_i.between?(1,9) && !position_taken?(board, input.to_i-1)
+def valid_move?(board, position)
+  if position.to_i.between?(1,9) 
+    if !position_taken?(board, position.to_i-1)
+      true
+    end
+  end
+
+  # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
 
 def turn(board)
   display_board(board)
   puts "Please enter 1-9:"
-  input = user_input
+  input = gets.strip
   if valid_move?(board, input)
     move(board, input)  
   else
@@ -24,14 +29,10 @@ def turn(board)
   end
 end
 
-def user_input
-  gets.strip
-end
-
 def position_taken?(board, location)
   board[location] != " "
 end
 
-def move(board, location)
-  board[location.to_i-1] = current_player(board)
+def move(board, location, current_player = "X")
+  board[location.to_i-1] = current_player
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,2 +1,49 @@
-require_relative "../lib/turn.rb"
+require_relative "../lib/tic_tac_toe.rb"
 
+describe './lib/tic_tac_toe.rb' do
+  describe '#turn' do
+    it 'asks the user for input' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+      allow($stdout).to receive(:puts)
+
+      expect(self).to receive(:gets).and_return("1")
+
+      turn(board)
+    end    
+
+    it 'validates the input correctly' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+      allow($stdout).to receive(:puts)
+
+      expect(self).to receive(:gets).and_return("1")
+      expect(self).to receive(:valid_move?).with(board, "1").and_return(true)
+
+      turn(board)      
+    end
+
+    it 'asks for input again after a failed validation' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+      allow($stdout).to receive(:puts)
+
+      expect(self).to receive(:gets).and_return("invalid")
+      expect(self).to receive(:gets).and_return("1")
+
+      turn(board)      
+    end
+
+    it 'makes valid moves' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+      allow($stdout).to receive(:puts)
+
+      expect(self).to receive(:gets).and_return("1")
+
+      turn(board)
+
+      expect(board).to match_array(["X", " ", " ", " ", " ", " ", " ", " ", " "])
+    end
+  end
+end
