08e0965410761145c022aa0cf441e1fa5a2f4019
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-05-13 16:54:04 -0400

    Add tests for input_to_index

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -27,6 +27,28 @@ describe './lib/turn.rb' do
     end
   end
 
+  describe '#input_to_index' do
+
+    it 'convervets a user_input to an integer' do
+      user_input = "1"
+
+      expect(input_to_index(user_input)).to be_a(Integer)
+    end
+
+    it 'subtracts 1 from the user_input' do
+      user_input = "6"
+
+      expect(input_to_index(user_input)).to be(5)
+    end
+
+    it 'returns -1 for strings without integers' do
+      user_input = "invalid"
+
+      expect(input_to_index(user_input)).to be(-1)
+    end
+
+  end
+
   describe '#valid_move?' do
     it 'returns true/false based on index' do
       board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
@@ -68,6 +90,17 @@ describe './lib/turn.rb' do
       allow($stdout).to receive(:puts)
 
       expect(self).to receive(:gets).and_return("1")
+
+      turn(board)
+    end
+
+    it 'calls the input_to_index method' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      allow($stdout).to receive(:puts)
+
+      allow(self).to receive(:gets).and_return("1")
+
+      expect(self).to receive(:input_to_index)
 
       turn(board)
     end
