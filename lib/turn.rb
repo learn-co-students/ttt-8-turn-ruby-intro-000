5edf43315c02fb5c79224019e16218b4dc497041
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-05-13 17:14:11 -0400

    Update solution to work with position and index

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,31 +6,36 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, position)
-  if position.to_i.between?(1,9)
-    if !position_taken?(board, position.to_i-1)
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
       true
     end
   end
 
-  # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+  # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
 end
 
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  if valid_move?(board, input)
-    move(board, input)
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
     display_board(board)
   else
     turn(board)
   end
 end
 
-def position_taken?(board, location)
-  board[location] != " "
+def position_taken?(board, index)
+  board[index] != " "
 end
 
-def move(board, location, current_player = "X")
-  board[location.to_i-1] = current_player
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
