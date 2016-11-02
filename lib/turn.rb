ade5486712b21f697078e2e629d7e7cd81f764fd
Michal Vacek <vahyno@gmail.com>
2016-11-02 00:51:35 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,58 +6,46 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your #valid_move? method here
-def valid_move?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil && index == (0..8)
-  true
-  else
-  false
-  end
-end
-
-
-def turn(board)
-  puts "Please enter 1-9:"
-  index = input_to_index(input)
-end
-
-# code your input_to_index and move method here!
 def input_to_index(input) #(user_input)
   input.to_i - 1          #user_input.to_i - 1
+end
+
+#def valid_move?(board, position)
+#  if position.to_i.between?(1,9)
+#  if !position_taken?(board, position.to_i-1)
+#  TRUE
+#  end
+#   end
+# position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+
+def valid_move?(board, index)
+  index != (-1) && index != (-2) && index != (-3) && index != (-4) && index != (-5) && index != (-6) && index != (-7) && index != (-8) && index != (-9) && board[index] == " " || board[index] == "" || board[index] == nil && index == (0..8)?
+  true : false
+  #if board[index] == " " || board[index] == "" || board[index] == nil && index == (0..8)
+  #true
+  #else
+  #false
+  #end
+end
+
+def turn(board)
+ puts "Please enter 1-9:"
+ input = gets.strip
+ index = input_to_index(input)
+ if valid_move?(board, index)
+ move(board, index)
+ else
+   print "invalid entry    "
+ turn(board)
+ end
+ puts ""
+ #display_board(board)
 end
 
 def move(board, index, current_player = "X")
   board[index] = current_player
 end
 
-def position_taken?(board, index)
-  #  taken = nil
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  #elsif (board[index] == "X" || board[index] == "O")
-  #  true
-  else
-    true # "invalid entry"
-  end
-#  (board[index] == " " || board[index] == "" || board[index] == nil) ?
-#  false : true
-end
-
-
-
-#  board[index] == " " || board[index] == "" || board[index] == nil && index == (0..8)?
-#  true : false
+#def position_taken?(board, location)
+# board[location] != " "
 #end
-
-input = gets.strip
-index = input_to_index(input)
-
-position_taken?(board, index)
-if position_taken?(board, index)
-  puts "invalid"
-  puts "Where would you like to go ?"
-  input = gets.strip
-  index = input_to_index(input)
-else
-  move(board, index)
-end
