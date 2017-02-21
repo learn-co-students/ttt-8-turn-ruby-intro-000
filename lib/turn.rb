faec2fc746bd95de9bb75705570f64991955b14e
Sally Whang <wsaehee21@outlook.com>
2017-02-21 21:20:10 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,38 +1,40 @@
-def display_board (board)
-  dashed_line = "-----------"
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts dashed_line
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts dashed_line
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def display_board(board)
+  dash = "-----------"
+  row_one = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row_two = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row_three = " #{board[6]} | #{board[7]} | #{board[8]} "
+  print "#{row_one}\n#{dash}\n#{row_two}\n#{dash}\n#{row_three}"
 end
 
-def turn (board)
+def input_to_index(index)
+  if index != " "
+    index.to_i - 1
+  else
+    index = -1
+  end
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && position_taken?(board, index) ? true : false
+end
+
+def position_taken?(board, index)
+  board[index] == " "
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+end
+
+def turn(board)
   puts "Please enter 1-9:"
+  display_board(board)
   input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
+  user_index = input_to_index(input)
+  if valid_move?(board, user_index)
+    move(board, user_index)
     display_board(board)
   else
     turn(board)
   end
-end
-
-def input_to_index (input)
-  input.to_i - 1
-end
-
-def valid_move?(board, index)
-  index.between?(0, 10) && !position_taken?(board, index) ? true : false
-end
-
-def position_taken? (board, index)
-  (board[index] == " " || board[index] == "" || board[index] == nil) ?
-    false : true
-end
-
-
-def move (board, index, player = "X")
-  board[index] = player
 end
