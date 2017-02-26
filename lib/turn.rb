b31b5b40da36bf8d92f2cf516fb4b20d755133af
Lillie Hutton <lillie.g.hutton@gmail.com>
2017-02-26 00:00:59 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,49 @@
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+      taken = nil
+    if board[index] == " " || board[index] == "" || board[index] == nil
+        taken = false
+    else
+        taken = true
+end
+
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+      turn(board)
+  end
+end
