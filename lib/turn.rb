2b974d9a39fd78883ef0a330b251207d321c522b
Nathaniel Richardson <richardson.j.nathaniel@gmail.com>
2017-04-08 19:53:36 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,54 @@
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, position, character="X")
+  board[position] = character
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == true || index.between?(0,8) == false
+     false
+   elsif position_taken?(board, index) == false && index.between?(0,8)
+     true
+   end
+ end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+
+end
+
+def move(board, position, character="X")
+  board[position] = character
+end
+
+
+
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+   input = gets.strip
+   index = input_to_index(input)
+   if valid_move?(board, index) == true
+     move(board, index)
+     display_board(board)
+   elsif valid_move?(board, index) == false
+     puts "Invalid move"
+     turn(board)
+
+   end
+ end
