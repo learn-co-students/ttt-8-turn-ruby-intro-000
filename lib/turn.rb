d834788250d553a2998a46acc3888bd509cf00c2
GG Learn <ozolsdeko@gmail.com>
2017-10-27 13:54:54 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,3 @@
-def turn(board)
-  puts "Please enter 1-9:"
-  position = input_to_index(gets.to_i)
-  if valid_move?(board, position)
-    move(board, position)
-    display_board(board)
-  else
-    puts "Please enter 1-9:"
-    position = input_to_index(gets.to_i)
-  end
-end
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -18,21 +6,34 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+
 def input_to_index(index)
   index.to_i - 1
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def valid_move?(board, index)
+  !position_taken?(board, index) &&  index.between?(0,8)
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  position = input_to_index(gets.to_i)
+  if valid_move?(board, position)
+    move(board, position)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
 
 def move(board, position, char="X")
   board[position] = char
   board
 end
 
-def valid_move?(board, index)
-  !position_taken?(board, index) &&  index.between?(0,8)
-end
 
 def position_taken?(board, index)
   board[index] != " "
