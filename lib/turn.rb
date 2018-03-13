ef5059b85615342f98bcd66ae693dd4765ef6c74
Al3nMicL <https://github.com/Al3nMicL>
2018-03-13 14:43:49 -0400

    the single #turn works as expected

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,3 +46,16 @@ end
 def move(board, index, x)
   board[index]=x
 end
+
+# allow the user to take a turn *TASK 5
+def turn(board)
+  puts "Please enter 1-9:"
+  @input = gets.strip
+  @index = input_to_index(@input)
+  if (valid_move?(board, @index))
+    move(board, @index, 'X')
+    display_board(board)
+  else
+    turn(board)
+  end
+end
