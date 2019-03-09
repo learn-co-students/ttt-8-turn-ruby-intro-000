14ecee2b59f26d87700e2c975ec8a36a1e8791c8
JENNIFER OH <ohjennifer.90@gmail.com>
2019-03-09 14:12:19 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,22 +17,22 @@ def move(board, index, character = "X")
 end
 
 
-def valid_move?(board, index)
-  if (index.between?(0,8) == true) && (position_taken?(board, index) == false)
-    true
+def position_taken?(board, index)
+  if !(board[index] == "" || board[index] == " " || board[index] == nil)
+    return true
   else
-    false
+    return false
   end
 end
 
 
- def position_taken?(board, index)
-   if board[index] == " " || board[index] == "" || board[index] == nil
-     false
-   else
-     true
-   end
- end
+def valid_move?(board, index)
+  if index.between?(0, 8) && !(position_taken?(board, index))
+    return true
+  else
+    return false
+  end
+end
 
 
 def turn(board)
