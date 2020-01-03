0230a97c1c0e955e88e0a9463ea4aef13de884bc
Qoosim <plenty4luv@yahoo.com>
2020-01-03 19:38:25 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " "
+    false
+  else
+    true
+  end
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, value = "X")
+    display_board(board)
+  else
+    puts "Invalid move!. Please try again"
+    turn(board)
+  end
+end
