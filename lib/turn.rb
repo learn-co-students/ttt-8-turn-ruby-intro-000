609f7085e5dce5a5c1cc36ecb069c25f547a68ac
Shari Damewood <sharidamewood3@gmail.com>
2020-05-31 13:57:25 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,28 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def position_taken?(board, index)
+  if board[index] == "   " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    true
+  else
+    false
+  end
+end
+
