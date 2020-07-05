35afa2590c1d7adc05296ad2e6e45f57cba583be
Corbin A <corbin_amos@yahoo.com>
2020-07-05 11:58:24 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,10 +3,11 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  if(board[index]=="X" || board[index]=="O")
+  if(board[index]=="X"||board[index]=="O")
     return true
   end
   return false
+
 end
 
 def position_on_board?(index)
