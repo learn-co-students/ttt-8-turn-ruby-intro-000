2282da6bc4337bd8622749cf3352d9f51d8c5009
Alexander Bowers <7319abowers@gmail.com>
2020-09-25 22:35:15 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,8 @@
 require "pry"
-def display_board (board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def greeting(input)
+puts "Please enter 1-9:"
+return input
 end
 
 def input_to_index(input)
@@ -13,9 +11,19 @@ def input_to_index(input)
   return index
 end
 
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+
 def position_available?(board, index)
-  binding.pry
-  if board[index] == "X" || "O"
+  #binding.pry
+  if board[index] == "X" || board[index] ==  "O"
     available = false
   else
     available = true
@@ -40,5 +48,17 @@ def move(board, index, character = "X")
   end
 end
 
-def turn(index)
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+
+  until valid_move?(board, index)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+  end
+  binding.pry
+  move(board, index, character = "X")
+  display_board(board)
 end
