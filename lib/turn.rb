d655a13b99035e7150e0aec49e1aa0c096d52bdc
Oluwaseyi Olufemi <oluwaseyi.olufemi.e@gmail.com>
2021-05-10 18:05:14 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,53 @@
+#print the board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+#converts user input_to_index
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+#valid_move
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+end
+#move
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+#turn
+def turn (board)
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  index = input_to_index(input)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
